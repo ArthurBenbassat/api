cdef46e71cc86c93677b8808eca9c194fb3fb67d
Arthur Benbassat <arthur.benbassat@outlook.com>
2020-04-16 14:48:20 +0200

    bugs gefixt

--- classes/businessProduct.php
+++ classes/businessProduct.php
@@ -5,4 +5,5 @@ class BusinessProduct {
     public $name;
     public $price;
     public $photo;
+    public $video;
 }
--- classes/dataProduct.php
+++ classes/dataProduct.php
@@ -12,11 +12,11 @@ class DataProduct {
     }
     public function read($id, $language) {
         if ($language == 'fr_FR') {
-            $sql = "SELECT pl.*, m.media as photo, p.price, b.name brand, p.weight, ca.name category, p.category_id from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'fr_FR' AND p.id = $id AND ca.language = 'fr_FR'";
+            $sql = "SELECT pl.*, m.media as photo, p.price, videos.media as video, b.name brand, p.weight, ca.name category, p.category_id from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id and m.media_type = 'standard photo' LEFT OUTER JOIN shop_media videos ON videos.product_id = p.id and videos.media_type = 'Video Lukas Lelie' INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'fr_FR' AND p.id = $id AND ca.language = 'fr_FR'";
         } elseif ($language == 'en_US') {
-            $sql = "SELECT pl.*, m.media as photo, p.price, b.name brand, p.weight, ca.name category, p.category_id from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'en_US' AND p.id = $id AND ca.language = 'en_US'";
+            $sql = "SELECT pl.*, m.media as photo, p.price, videos.media as video, b.name brand, p.weight, ca.name category, p.category_id from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id and m.media_type = 'standard photo' LEFT OUTER JOIN shop_media videos ON videos.product_id = p.id and videos.media_type = 'Video Lukas Lelie' INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'en_US' AND p.id = $id AND ca.language = 'en_US'";
         } elseif ($language == 'nl_BE') {
-            $sql = "SELECT m.media photo, p.name, p.price, p.id, b.name brand, p.weight, p.ingredients, ca.name category, p.category_id FROM shop_products p INNER JOIN shop_categories ca ON ca.id = p.category_id  left outer JOIN shop_media m ON m.id = p.media_id INNER JOIN shop_brands b ON p.brand_id = b.id WHERE p.id = $id;";
+            $sql = "SELECT m.media photo, p.name, p.price, videos.media as video, p.id, b.name brand, p.weight, p.ingredients, ca.name category, p.category_id FROM shop_products p INNER JOIN shop_categories ca ON ca.id = p.category_id  left outer JOIN shop_media m ON m.id = p.media_id and m.media_type = 'standard photo' LEFT OUTER JOIN shop_media videos ON videos.product_id = p.id and videos.media_type = 'Video Lukas Lelie' INNER JOIN shop_brands b ON p.brand_id = b.id WHERE p.id = $id;";
         } else {
             throw new Exception('Language not recognized');
         }
@@ -27,6 +27,7 @@ class DataProduct {
             $product = new BusinessProduct();
             $product->id = $row['id'];
             $product->photo = $row['photo'];
+            $product->video = $row['video'];
             $product->name = $row['name'];
             $product->price = $row['price'];
             $product->brand = $row['brand'];
@@ -43,11 +44,11 @@ class DataProduct {
     }
     public function readAll($language, $where = "", $orderBy = "") {
         if ($language == 'fr_FR') {
-            $sql = "SELECT pl.*, m.media as photo, p.price, b.name brand, p.weight, ca.name category from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'fr_FR' AND ca.language = 'fr_FR' $where $orderBy";
+            $sql = "SELECT pl.*, m.media as photo, videos.media as video, p.price, b.name brand, p.weight, ca.name category from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id and m.media_type = 'standard photo' LEFT OUTER JOIN shop_media videos ON videos.product_id = p.id and videos.media_type = 'Video Lukas Lelie' INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'fr_FR' AND ca.language = 'fr_FR' $where $orderBy";
         } elseif ($language == 'en_US') {
-            $sql = "SELECT pl.*, m.media photo, p.price, b.name brand, p.weight, ca.name category from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'en_US' AND ca.language= 'en_US' $where $orderBy";
+            $sql = "SELECT pl.*, m.media photo, videos.media as video, p.price, b.name brand, p.weight, ca.name category from shop_products_lang pl left outer join shop_products p on pl.id = p.id Left outer JOIN shop_media m ON m.id = p.media_id and m.media_type = 'standard photo' LEFT OUTER JOIN shop_media videos ON videos.product_id = p.id and videos.media_type = 'Video Lukas Lelie' INNER JOIN shop_categories_lang ca ON ca.id = p.category_id INNER JOIN shop_brands b ON p.brand_id = b.id where pl.language = 'en_US' AND ca.language= 'en_US' $where $orderBy";
         } elseif ($language == 'nl_BE') {
-            $sql = "SELECT m.media photo, p.name, p.price, p.id, p.weight, p.ingredients, b.name brand, ca.name category FROM shop_products p  INNER JOIN shop_categories ca ON ca.id = p.category_id Left outer JOIN shop_media m ON m.id = p.media_id INNER JOIN shop_brands b ON p.brand_id = b.id WHERE p.name <> '' $where $orderBy;";
+            $sql = "SELECT m.media photo, p.name, videos.media as video, p.price, p.id, p.weight, p.ingredients, b.name brand, ca.name category FROM shop_products p  INNER JOIN shop_categories ca ON ca.id = p.category_id Left outer JOIN shop_media m ON m.id = p.media_id and m.media_type = 'standard photo' LEFT OUTER JOIN shop_media videos ON videos.product_id = p.id and videos.media_type = 'Video Lukas Lelie' INNER JOIN shop_brands b ON p.brand_id = b.id WHERE p.name <> '' $where $orderBy;";
         } else {
             throw new Exception('Language not recognized');
         }
@@ -60,6 +61,7 @@ class DataProduct {
             $product = new BusinessProduct();
             $product->id = $row['id'];
             $product->photo = $row['photo'];
+            $product->video = $row['video'];
             $product->name = $row['name'];
             $product->price = $row['price'];
             $product->brand = $row['brand'];
